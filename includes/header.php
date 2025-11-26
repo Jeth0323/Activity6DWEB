66aeeab5b6ece8b047cb0bd70a6919fc3e888c26
JethroDizon <166384316+Jeth0323@users.noreply.github.com>
2025-11-26 14:03:18 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -7,10 +7,11 @@
     <body>
 <h1>The Chocolate Factory</h1>
 <nav>
-    <a href ='candy.php'>Home</a>
-    <a href ='candy.php'>Candy</a>
-    <a href ='candy.php'>About</a>
-    <a href ='candy.php'>Contact</a>
+    <a href ='includes.php'>Home</a>
+    <a href ='includes.php'>Candy</a>
+    <a href ='includes.php'>About</a>
+    <a href ='includes.php'>Contact</a>
 </nav>
 
     </body>
+
